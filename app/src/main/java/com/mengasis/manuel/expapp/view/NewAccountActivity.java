53047b5666581596f0826d8a13e0aa387dda9a22
Manuel Venegas <mengasis@outlook.com>
2016-12-05 19:18:20 -0300

    Se añadio la actividad para crear una cuenta

---
+++ app/src/main/java/com/mengasis/manuel/expapp/view/NewAccountActivity.java
@@ -0,0 +1,15 @@
+package com.mengasis.manuel.expapp.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mengasis.manuel.expapp.R;
+
+public class NewAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_account);
+    }
+}
