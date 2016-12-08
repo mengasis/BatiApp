b6e84e5eec9cbebe1100182559f9296a3edea75c
Manuel Venegas <mengasis@outlook.com>
2016-12-08 16:02:23 -0300

    Generada la vista del formulario para crear cuenta junto con la funcion para inicializar el toolbar

--- app/src/main/java/com/mengasis/manuel/expapp/view/NewAccountActivity.java
+++ app/src/main/java/com/mengasis/manuel/expapp/view/NewAccountActivity.java
@@ -2,6 +2,7 @@ package com.mengasis.manuel.expapp.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.mengasis.manuel.expapp.R;
 
@@ -11,5 +12,17 @@ public class NewAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_account);
+
+        showToolbar(getResources().getString(R.string.titulo_crear_cuenta),true);
+    }
+
+    // Recibira un titulo y un boton de regreso
+    public void showToolbar(String titulo, Boolean backButton){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(titulo);
+
+        // Activa o desactiva el boton de regreso
+        getSupportActionBar().setDisplayHomeAsUpEnabled(backButton);
     }
 }
